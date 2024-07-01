3cc5c3e2af9b39a29de18e43dd5f6f77c2e3c28f
tabrizihamid84 <tabrizihamid6367@gmail.com>
2024-07-01 23:28:22 +0330

    33

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
+
+const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "anson2", displayName: "Anson2" },
+  { id: 3, username: "anson3", displayName: "Anson3" },
+  { id: 4, username: "anson4", displayName: "Anson4" },
+];
 
 app.get("/", (req, res) => {
   res.send("lllll");
